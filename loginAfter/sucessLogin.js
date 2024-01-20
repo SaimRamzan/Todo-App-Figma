66881e1afc8fd48aa589b9507d8359a430d66358
SaimRamzan <Saimshinwari001@gmail.com>
2024-01-20 19:09:38 +0500

    reminder properly working

--- loginAfter/sucessLogin.js
+++ loginAfter/sucessLogin.js
@@ -10,7 +10,7 @@ const task = document.getElementById('incomleteTask')
 const logout = document.getElementById('logout')
 const loaction = document.querySelector('.loaction')
 const skipButton = document.querySelector('.skipp')
-
+const completeTask = document.getElementById('completeTask')
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
@@ -72,6 +72,60 @@ const incompleteTask = () => {
     })
 }
 
+// console.log('data',data);
+
+// const completedData = []
+// const incompleteTaskDelete = ()=>{
+//     // setInterval(()=>{
+//         const localDate = new Date
+//         const gethours = localDate.getHours()
+//         const getMinutes = localDate.getMinutes()
+//         const concatMinHours = `${gethours}:${getMinutes}`;
+//         const filterData = data.filter(item => item.date === concatMinHours);
+//         console.log('filterData',filterData);
+//         completedData.push(filterData)
+//         completeTask.innerHTML ='';
+//     let completeDiv = document.createElement('div')
+//     completeDiv.innerHTML=`
+//          <div class="completeTask"  id="completeTask">
+//             <input type="checkbox" checked name="checkbox" id="checkboxComplete">
+//             <h3>${data.text}</h3>
+//             </div>`
+//             completeTask.appendChild(completeDiv)
+// }
+// console.log('completedData',completedData);
+
+
+// const completedData = [];
+// const incompleteTaskDelete = () => {
+//     const localDate = new Date();
+//     const gethours = localDate.getHours();
+//     const getMinutes = localDate.getMinutes();
+//     const concatMinHours = `${gethours}:${getMinutes}`;
+    
+//     // Assuming data is an array of objects with a 'date' property
+//     const filterData = data.filter(item => item.date === concatMinHours);
+
+//     console.log('filterData', filterData);
+
+//     // Assuming completeTask is a DOM element
+//     completeTask.innerHTML = '';
+
+//     filterData.forEach(item => {
+//         let completeDiv = document.createElement('div');
+//         completeDiv.innerHTML = `
+//             <div class="completeTask" id="completeTask">
+//                 <input type="checkbox" checked name="checkbox" id="checkboxComplete">
+//                 <h3>${item.text}</h3>
+//             </div>`;
+//         completeTask.appendChild(completeDiv);
+//     });
+
+//     completedData.push(filterData);
+// };
+// incompleteTaskDelete()
+// console.log('completedData', completedData);
+
 
 
 const formReset = ()=>{
@@ -87,29 +141,43 @@ const checkbox = () => {
         console.log('hye');
     }
 }
+let clear ;
+let reminderEnable = true ;
+
+
+// array => incomplete
+// => filter it according to reminderd tasks
+// delete the same task
+// let deleteItem = fpokdop
+
+// newarray => completed []
+// newarray.push()
+    // push deleted item => deleteItem
+
+
 
 const reminder =  ()=>{
-    setInterval(() => {
-        const getDate = data.filter(item => item.date)
-    const localDate = new Date
-    const gethours = localDate.getHours()
-    const getMinutes = localDate.getMinutes()
-    const concatMinHours = `${gethours}:${getMinutes}`;
-    getDate.filter(item => {
-        if (item.date === concatMinHours) {
-            const remminder = document.getElementById('main-header');
-            const headText = document.querySelector('.reminder');
-            const decription = document.getElementById('remindAbout');
-            headText.innerText = item.text;
-            decription.innerText = item.decription;
-            remminder.style.display = 'block'
-            
-        }else{
-            const remminder = document.getElementById('main-header');
-            remminder.style.display = 'none'
+    clear = setInterval(() => {
+        if (reminderEnable) {
+            const getDate = data.filter(item => item.date)
+            const localDate = new Date
+            const gethours = localDate.getHours()
+            const getMinutes = localDate.getMinutes()
+            const concatMinHours = `${gethours}:${getMinutes}`;
+            getDate.filter(item => {
+                const remminder = document.getElementById('main-header');
+                        const headText = document.querySelector('.reminder');
+                        const decription = document.getElementById('remindAbout');
+                    if (item.date === concatMinHours) {
+                        headText.innerText = item.text;
+                        decription.innerText = item.decription;
+                        remminder.style.display = 'block';  
+                    }else{
+                        remminder.style.display = 'none'
+                    }
+        
+            })
         }
-
-    })
     }, 10000);
     
 };
@@ -117,7 +185,15 @@ const reminder =  ()=>{
 
 skipButton.addEventListener('click', () => {
     const remminder = document.getElementById('main-header')
-    remminder.style.display = 'none'});
+    remminder.style.display = 'none'
+
+    reminderEnable = false
+    clearInterval(clear)
+
+    setTimeout(()=>{
+        reminderEnable = true
+    },50000)
+});
 
 (()=>{
     data = JSON.parse(localStorage.getItem("data")) || []

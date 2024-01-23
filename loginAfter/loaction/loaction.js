e12c211f0c7593ed892d6138aa865b747a9e1b4f
SaimRamzan <Saimshinwari001@gmail.com>
2024-01-23 18:04:14 +0500

    All Data save in local Storage

--- loginAfter/loaction/loaction.js
+++ loginAfter/loaction/loaction.js
@@ -1,133 +1,126 @@
-const task = document.querySelector('.task');
-const logout = document.getElementById('logout');
-const checkIn = document.querySelector('.addTask');
-const locationn =document.getElementById('incomleteTask'); 
+const task = document.querySelector(".task");
+const logout = document.getElementById("logout");
+const checkIn = document.querySelector(".addTask");
+const locationn = document.getElementById("incomleteTask");
 
+const loactionActive = document.querySelector(".loaction");
 
-
-const loactionActive = document.querySelector('.loaction')
-
-let active = ()=>{
-    if (loactionActive == true) {
-        loactionActive.style.backgroundColor = "white"
-    }else{
-        loactionActive.style.backgroundColor = "black"
-        loactionActive.style.color = "white"
-    
-    }
-}
-active()
+let active = () => {
+  if (loactionActive == true) {
+    loactionActive.style.backgroundColor = "white";
+  } else {
+    loactionActive.style.backgroundColor = "black";
+    loactionActive.style.color = "white";
+  }
+};
+active();
 
 function getSuccessLocation(position) {
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
-    console.log('latitude:', latitude,'N','longitude:', longitude,'E', );
-     const wetherApi = `https://api.weatherapi.com/v1/current.json?key=c5ada4e27ed847fe9f8110605240801&q=${latitude},${longitude}&aqi=yes`;
-   
-    apiFun(wetherApi).then(data => {
-        const city = data.location.name || "Unknown City";
-        const region = data.location.region
-        const tzId = data.location.tz_id
-        console.log('city',city);
-        const loactionData = {
-            city:city,
-            region:region,
-            tzId:tzId,
-            latitude:latitude,
-            longitude:longitude,
-        }
-        localStorage.setItem("loactionData",JSON.stringify(loactionData))
-        previousLocation();
-    let createElement = document.createElement('div')
-    createElement.innerHTML = `
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log("latitude:", latitude, "N", "longitude:", longitude, "E");
+  const wetherApi = `https://api.weatherapi.com/v1/current.json?key=c5ada4e27ed847fe9f8110605240801&q=${latitude},${longitude}&aqi=yes`;
+
+  apiFun(wetherApi)
+    .then((data) => {
+      const city = data.location.name || "Unknown City";
+      const region = data.location.region;
+      const tzId = data.location.tz_id;
+      console.log("city", city);
+      const loactionData = {
+        city: city,
+        region: region,
+        tzId: tzId,
+        latitude: latitude,
+        longitude: longitude,
+      };
+      localStorage.setItem("loactionData", JSON.stringify(loactionData));
+      previousLocation();
+      let createElement = document.createElement("div");
+      createElement.innerHTML = `
      <p class="checkbox"  id="">📍</p>
         <h3>${city},s ${region}, ${tzId}</h3>
         <h5>${latitude}° N, ${longitude}° E</h5>
-    `
-    locationn.appendChild(createElement)
-})
-.catch(error => console.error('Error fetching weather data:', error));
-
-
+    `;
+      locationn.appendChild(createElement);
+    })
+    .catch((error) => console.error("Error fetching weather data:", error));
 }
 function getFailedLocation() {
-    alert('Some issue to get you Location ( allow location access ) ');
+  alert("Some issue to get you Location ( allow location access ) ");
 }
 
-let apiFun = async (url)=>{
-    try {
-        let result = await fetch(url);
-        let data = await result.json();
-        console.log(data);
-        return data
-    } catch (error) {
-    console.error( error);
-    throw error
-    }
-    
-}
+let apiFun = async (url) => {
+  try {
+    let result = await fetch(url);
+    let data = await result.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
+checkIn.addEventListener("click", async () => {
+  navigator.geolocation.getCurrentPosition(
+    getSuccessLocation,
+    getFailedLocation
+  );
+});
 
-checkIn.addEventListener('click', async ()=>{
-   navigator.geolocation.getCurrentPosition(getSuccessLocation, getFailedLocation)
-})
-
-const previousLocation = ()=>{
-   const completeTask = document.querySelector('.completeTask')
-    const currentDiv = document.createElement('div')
-    setTimeout(()=>{
-        const loactionData = localStorage.getItem("loactionData");
-        if (loactionData) {
-            const dataParse = JSON.parse(loactionData);
-            currentDiv.innerHTML= `
+const previousLocation = () => {
+  const completeTask = document.querySelector(".completeTask");
+  const currentDiv = document.createElement("div");
+  setTimeout(() => {
+    const loactionData = localStorage.getItem("loactionData");
+    if (loactionData) {
+      const dataParse = JSON.parse(loactionData);
+      currentDiv.innerHTML = `
             <p class="checkbox"  id="">📍</p>
             <h3>${dataParse.city},s ${dataParse.region}, ${dataParse.tzId}</h3>
             <h5>${dataParse.latitude}° N, ${dataParse.longitude}° E</h5>
         `;
-        completeTask.appendChild(currentDiv)
-        removeChild(getSuccessLocation)
-        localStorage.setItem("previousLocation", JSON.stringify(currentDiv.innerHTML))
-        }
-       
-    },2000) 
-
-} 
-task.addEventListener('click', () => {
-    window.location.href = "../sucessLogin.html";
-
+      localStorage.setItem("previousLocation", JSON.stringify(dataParse));
+      completeTask.appendChild(currentDiv);
+      removeChild(getSuccessLocation);
+      localStorage.setItem(
+        "previousLocation",
+        JSON.stringify(currentDiv.innerHTML)
+      );
+    }
+  }, 2000);
+};
+task.addEventListener("click", () => {
+  window.location.href = "../sucessLogin.html";
 });
-logout.addEventListener("click", ()=>{
-    window.location.href = "../../index.html"
+logout.addEventListener("click", () => {
+  window.location.href = "../../index.html";
 });
 
-
-(()=>{
-const loactionData = localStorage.getItem("loactionData") 
-    if (loactionData) {
-        const dataParse = JSON.parse(loactionData);
-        let createElement = document.createElement('div')
-         createElement.innerHTML = `
+(() => {
+  const loactionData = localStorage.getItem("loactionData");
+  if (loactionData) {
+    const dataParse = JSON.parse(loactionData);
+    let createElement = document.createElement("div");
+    createElement.innerHTML = `
      <p class="checkbox"  id="">📍</p>
         <h3>${dataParse.city},s ${dataParse.region}, ${dataParse.tzId}</h3>
         <h5>${dataParse.latitude}° N, ${dataParse.longitude}° E</h5>
-    `
-    locationn.appendChild(createElement)
-    };
+    `;
+    locationn.appendChild(createElement);
+  }
 
-
-    const previousLocationData =localStorage.getItem("previousLocation")
-if (previousLocationData) {
+  const previousLocationData = localStorage.getItem("previousLocation");
+  if (previousLocationData) {
     const dataParse = JSON.parse(loactionData);
-    const completeTask = document.querySelector('.completeTask')
-    const currentDiv = document.createElement('div')
-    currentDiv.innerHTML =`
+    const completeTask = document.querySelector(".completeTask");
+    const currentDiv = document.createElement("div");
+    currentDiv.innerHTML = `
     <p class="checkbox"  id="">📍</p>
         <h3>${dataParse.city},s ${dataParse.region}, ${dataParse.tzId}</h3>
         <h5>${dataParse.latitude}° N, ${dataParse.longitude}° E</h5>
-    `
-    completeTask.appendChild(currentDiv)
-}
-
-
+    `;
+    completeTask.appendChild(currentDiv);
+  }
 })();
-
--- loginAfter/sucessLogin.js
+++ loginAfter/sucessLogin.js
@@ -1,188 +1,197 @@
-const form = document.getElementById('form')
-const submit = document.getElementById('add')
-const input= document.getElementById('input')
-const inputMessage = document.getElementById('inputMessage')
-const decription= document.getElementById('decription')
-const inputDec = document.getElementById('inputDec')
-const date= document.getElementById('date')
-const inputdate = document.getElementById('inputdate')
-const task = document.getElementById('incomleteTask')
-const logout = document.getElementById('logout')
-const loaction = document.querySelector('.loaction')
-const skipButton = document.querySelector('.skipp')
-const teskActive = document.querySelector('.task')
+const form = document.getElementById("form");
+const submit = document.getElementById("add");
+const input = document.getElementById("input");
+const inputMessage = document.getElementById("inputMessage");
+const decription = document.getElementById("decription");
+const inputDec = document.getElementById("inputDec");
+const date = document.getElementById("date");
+const inputdate = document.getElementById("inputdate");
+const task = document.getElementById("incomleteTask");
+const logout = document.getElementById("logout");
+const loaction = document.querySelector(".loaction");
+const skipButton = document.querySelector(".skipp");
+const teskActive = document.querySelector(".task");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
 
+const formValidation = () => {
+  if (input.value === "") {
+    inputMessage.innerHTML = "Input Filed Empty";
+    inputMessage.style.color = "Red";
+  } else if (decription.value === "") {
+    inputDec.innerHTML = "Decription Empty";
+    inputDec.style.color = "Red";
+  } else if (date.value === "") {
+    inputdate.innerHTML = "kindly added date";
+    inputdate.style.color = "Red";
+  } else {
+    inputMessage.innerHTML = "";
+    inputDec.innerHTML = "";
+    inputdate.innerHTML = "";
+    addData();
+    add.setAttribute("data-dismiss", "modal");
+    add.click();
+    closeModel();
+  }
+};
+let closeModel = () => {
+  add.setAttribute("data-dismiss", "");
+};
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    formValidation()
-})
+let data = [];
 
-const formValidation = ()=>{
-    if (input.value === '' ) {
-        inputMessage.innerHTML ='Input Filed Empty'
-        inputMessage.style.color ='Red'
-    }else if (decription.value === ''){
-        inputDec.innerHTML ='Decription Empty'
-        inputDec.style.color ='Red'
-    }else if (date.value === ''){
-        inputdate.innerHTML ='kindly added date'
-        inputdate.style.color ='Red'
-    }else{
-        inputMessage.innerHTML = '';
-        inputDec.innerHTML = '';
-        inputdate.innerHTML = '';
-        addData()
-        add.setAttribute("data-dismiss","modal")
-        add.click()
-        closeModel()
-    }
-}
-let closeModel = ()=>{
-    add.setAttribute("data-dismiss","")
-}
-
-let data = []
-
-let addData = ()=>{
-    data.push({
-        text :input.value,
-        decription : decription.value,
-        date: date.value,
-    })
-    localStorage.setItem("data", JSON.stringify(data))
-    console.log('data',data);
-    incompleteTask()
-}
+let addData = () => {
+  data.push({
+    text: input.value,
+    decription: decription.value,
+    date: date.value,
+  });
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log("data", data);
+  incompleteTask();
+};
 const incompleteTask = () => {
-     task.innerHTML = "";
-    data.map((a,b)=>{
-    const updatedTask = document.createElement('div');
-       updatedTask.innerHTML = `
+  task.innerHTML = "";
+  data.map((a, b) => {
+    const updatedTask = document.createElement("div");
+    updatedTask.innerHTML = `
        <div id=${b} class="incomleteTask" id="incomleteTask">
                         <input onclick="checkbox" class="checkbox" type="checkbox" name="checkbox" id="">
                         <h3>${a.text}</h3>
                         <h5>⏰ ${a.date}</h5>
         </div>
         `;
-        reminder();
-        updatedTask.style.marginBottom = "20px";
-        task.appendChild(updatedTask);
-        formReset();
-        incompleteTaskDelete()
-
-        
-    })
-}
-
-
-
-const formReset = ()=>{
-    input.value = ''
-    decription.value = ''
-    date.value= ''
-}
-const checkbox = () => {
-    const incompleteCheckBox = document.getElementById('checkbox').checked;
-    if (incompleteCheckBox === true) {
-        console.log('hello world');
-    } else {
-        console.log('hye');
-    }
-}
-let clear ;
-let reminderEnable = true ;
-
-const reminder =  ()=>{ 
-    clear = setInterval(() => {
-        if (reminderEnable) {
-            const getDate = data.filter(item => item.date)
-            const localDate = new Date
-            const gethours = localDate.getHours()
-            const getMinutes = localDate.getMinutes()
-            const concatMinHours = `${gethours}:${getMinutes}`;
-            getDate.map(item => {
-                const remminder = document.getElementById('main-header');
-                        const headText = document.querySelector('.reminder');
-                        const decription = document.getElementById('remindAbout');
-                    if (item.date === concatMinHours) {
-                        headText.innerText = item.text;
-                        decription.innerText = item.decription;
-                        remminder.style.display = 'block';  
-                    }else{
-                        remminder.style.display = 'none'
-                    }
-        
-            })
-        }   
-    }, 10000);
-    
+    reminder();
+    incompleteTaskDelete(a, b);
+    updatedTask.style.marginBottom = "20px";
+    task.appendChild(updatedTask);
+    formReset();
+  });
 };
 
+const formReset = () => {
+  input.value = "";
+  decription.value = "";
+  date.value = "";
+};
+const checkbox = () => {
+  const incompleteCheckBox = document.getElementById("checkbox").checked;
+  if (incompleteCheckBox === true) {
+    console.log("hello world");
+  } else {
+    console.log("hye");
+  }
+};
+let clear;
+let reminderEnable = true;
 
-skipButton.addEventListener('click', () => {
-    const remminder = document.getElementById('main-header')
-    remminder.style.display = 'none'
+const reminder = () => {
+  clear = setInterval(() => {
+    if (reminderEnable) {
+      const getDate = data.filter((item) => item.date);
+      const localDate = new Date();
+      const gethours = localDate.getHours();
+      const getMinutes = localDate.getMinutes();
+      const concatMinHours = `${gethours}:${getMinutes}`;
+      getDate.map((item) => {
+        const remminder = document.getElementById("main-header");
+        const headText = document.querySelector(".reminder");
+        const decription = document.getElementById("remindAbout");
+        if (item.date === concatMinHours) {
+          headText.innerText = item.text;
+          decription.innerText = item.decription;
+          remminder.style.display = "block";
+        } else {
+          remminder.style.display = "none";
+        }
+      });
+    }
+  }, 10000);
+};
 
-    reminderEnable = false
-    clearInterval(clear)
+skipButton.addEventListener("click", () => {
+  const remminder = document.getElementById("main-header");
+  remminder.style.display = "none";
 
-    setTimeout(()=>{
-        reminderEnable = true
-    },30000)
+  reminderEnable = false;
+  clearInterval(clear);
+
+  setTimeout(() => {
+    reminderEnable = true;
+  }, 30000);
 });
 
-const completedData = []
-const incompleteTaskDelete = (taskData, index)=>{
-    let checkAndComplete = ()=>{
-        const completeTask = document.getElementById('completeTaskk')
-        const localDate = new Date
-        const gethours = localDate.getHours()
-        const getMinutes = localDate.getMinutes()
-        const concatMinHours = `${gethours}:${getMinutes}`;
-        // const filterData = data.filter(item => item.date === concatMinHours);
-        const filterData = data.filter(item => item.date === concatMinHours && item.text === taskData.text);
+const completedData = [];
+let addDataA = () => {
+  completedData.push({
+    text: text,
+    decription: decription,
+    date: date,
+  });
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log("data", data);
+  incompleteTaskDelete();
+};
+const incompleteTaskDelete = (taskData, index) => {
+  let checkAndComplete = () => {
+    const completeTask = document.getElementById("completeTaskk");
+    const localDate = new Date();
+    const gethours = localDate.getHours();
+    const getMinutes = localDate.getMinutes();
+    const concatMinHours = `${gethours}:${getMinutes}`;
 
-        console.log('filterData',filterData);
-        filterData.map(item =>{
-            let completeDiv = document.createElement('div')
-           completeDiv.innerHTML=`
-             <div class="completeTask"  id="completeTask">
-                <input type="checkbox" checked name="checkbox" id="checkboxComplete">
-                <h3>${item.text}</h3>
-                </div>`;
-                completeTask.appendChild(completeDiv)
-        })
-        completedData.push(filterData)
+    const filterData = data.filter(
+      (item) => item.date === concatMinHours && item.text === taskData.text
+    );
+    console.log("filterData", filterData);
+    if (filterData.length > 0) {
+      filterData.map((item) => {
+        let completeDiv = document.createElement("div");
+        completeDiv.innerHTML = `
+                         <div class="completeTask"  id="completeTask">
+                            <input type="checkbox" checked name="checkbox" id="checkboxComplete">
+                            <h3>${item.text}</h3>
+                            </div>`;
+        completeTask.appendChild(completeDiv);
+
+        // incompleteTask.removeChild()
+        completedData.push(...filterData);
+      });
+      const taskDataRemove = data.findIndex((item) => item <= taskData);
+      if (taskDataRemove !== -1) {
+        data.splice(taskDataRemove, 1);
+      }
     }
-    setTimeout(checkAndComplete,10000)
-    checkAndComplete()
-}
-console.log('completedData',completedData);
+  };
+  checkAndComplete();
+};
+console.log("completedData", completedData);
 
-
-
-
-let active = ()=>{
-    if (teskActive == true ) {
-        teskActive.style.backgroundColor = "white"
-    }else{
-        teskActive.style.backgroundColor = "black"
-        teskActive.style.color = "white"
-    
-    }
-}
+let active = () => {
+  if (teskActive == true) {
+    teskActive.style.backgroundColor = "white";
+  } else {
+    teskActive.style.backgroundColor = "black";
+    teskActive.style.color = "white";
+  }
+};
 active();
 
-(()=>{
-    data = JSON.parse(localStorage.getItem("data")) || []
-    incompleteTask()
-})()
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  incompleteTask();
+})();
+(() => {
+  data = JSON.parse(localStorage.getItem("completedData")) || [];
+  incompleteTaskDelete();
+})();
 
-loaction.addEventListener('click',()=>{
-    window.location.href ='loaction/loaction.html'
-})
-logout.addEventListener("click", ()=>{
-    window.location.href = "../index.html"
-})
+loaction.addEventListener("click", () => {
+  window.location.href = "loaction/loaction.html";
+});
+logout.addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
